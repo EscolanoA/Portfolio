b71b729cebe6dbda013bfdb2fb785ddba0e001ff
EscolanoA <escolanoallan4@gmail.com>
2025-04-06 12:13:46 +0200

    Insane navbar making all scratch i'm so happy yipee

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,16 @@
 function switchTextWithDelay(delay) {
-    const texts = ["BIENVENUE ( 0_0)", "BIENVENUE ( ^_^)", "BIENVENUE ( ^_^) /", "BIENVENUE ( ^_^) |", "BIENVENUE ( ^_^) /", "BIENVENUE ( ^_^)", "BIENVENUE ( 0_0)"];
-    const textContainer = document.getElementById("emoji")
+    let texts = ["BIENVENUE ( 0_0)", "BIENVENUE ( ^_^)", "BIENVENUE ( ^_^) /", "BIENVENUE ( ^_^) |", "BIENVENUE ( ^_^) /", "BIENVENUE ( ^_^)", "BIENVENUE ( 0_0)"];
+    let pcTextContainer = document.getElementById("pcEmoji")
+    let mobileTextContainer = document.getElementById("mobileEmoji")
 
-    textContainer.innerHTML = "BIENVENUE ( 0_0)"
+    pcTextContainer.innerHTML = "BIENVENUE ( 0_0)"
+    mobileTextContainer.innerHTML = "BIENVENUE ( 0_0)"
 
     let index = 0;
 
     setInterval(() => {
-        textContainer.textContent = texts[index];
+        pcTextContainer.textContent = texts[index]
+        mobileTextContainer.textContent = texts[index]
         index = (index + 1) % texts.length;
     }, delay);
 }
@@ -15,11 +18,58 @@ function switchTextWithDelay(delay) {
 switchTextWithDelay(500);
 
 let pcnavbar = document.getElementById("pcnavbar")
+let mobilenavbar = document.getElementById("mobilenavbar")
 
 window.addEventListener('scroll',() => {
     if (window.scrollY > 0) {
         pcnavbar.classList.add('scrolled')
+        mobilenavbar.classList.add('scrolled')
     } else {
         pcnavbar.classList.remove('scrolled')
+        mobilenavbar.classList.remove('scrolled')
+    }
+})
+
+let menubutton = document.getElementById("menubutton")
+let extendframe = document.getElementById("extendframe")
+let extendframetop = document.getElementById("top")
+let extendframecenter = document.getElementById("center")
+let extendframebottom = document.getElementById("bottom")
+menubutton.addEventListener('click', () => {
+    if(mobilenavbar.classList.contains('extended')) {
+        //On closing
+        mobilenavbar.classList.remove('extended')
+
+        menubutton.classList.remove("spinOpen")
+        menubutton.classList.add("spinClose")
+
+        menubutton.classList.remove("close")
+        menubutton.classList.add("open")
+
+        extendframe.classList.remove("extended")
+
+        extendframecenter.classList.remove("show")
+        extendframebottom.classList.remove("show")
+
+        extendframetop.style.display ="none"
+        extendframecenter.style.display ="none"
+        extendframebottom.style.display ="none"
+    } else {
+        //On opening
+        mobilenavbar.classList.add('extended')
+
+        menubutton.classList.remove("spinClose")
+        menubutton.classList.add("spinOpen")
+
+        menubutton.classList.remove("open")
+        menubutton.classList.add("close")
+
+        extendframe.classList.add("extended")
+
+        extendframetop.style.display ="unset"
+        extendframecenter.style.display =""
+        extendframecenter.classList.add("show")
+        extendframebottom.style.display =""
+        extendframebottom.classList.add("show")
     }
 })
