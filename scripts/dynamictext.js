dbb97aa6dee52d238bb8ec62f3c5257b8b316af6
EscolanoA <escolanoallan4@gmail.com>
2025-04-13 17:30:33 +0200

    Age fix test

--- scripts/dynamictext.js
+++ scripts/dynamictext.js
@@ -1,18 +1,17 @@
-let ageplaceholder = document.getElementById("age")
+const ageplaceholder = document.getElementById("age");
 
-function setAge(birthdate) {
-    const today = new Date();
-    const birth = new Date(2003, 4, 30);
-    
-    let age = today.getFullYear() - birth.getFullYear();
-    const m = today.getMonth() - birth.getMonth();
-  
-    // If birth month hasn't occurred yet this year or it's the same month but birth day hasn't passed
-    if (m < 0 || (m === 0 && today.getDate() < birth.getDate())) {
-      age--;
-    }
-  
-    return age;
+function setAge() {
+  const today = new Date();
+  const birth = new Date(2003, 4, 30);
+
+  let age = today.getFullYear() - birth.getFullYear();
+  const m = today.getMonth() - birth.getMonth();
+
+  if (m < 0 || (m === 0 && today.getDate() < birth.getDate())) {
+    age--;
   }
 
-  ageplaceholder.innerHTML = setAge()
+  return age;
+}
+
+ageplaceholder.textContent = setAge();
