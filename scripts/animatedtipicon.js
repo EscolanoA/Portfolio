75a9ba2737c8efda540e5e3baeaa24e86ca252aa
EscolanoA <escolanoallan4@gmail.com>
2025-08-12 21:43:48 +0200

    footer fixes and styling

--- scripts/animatedtipicon.js
+++ scripts/animatedtipicon.js
@@ -1,4 +1,5 @@
 const techtipIcon = document.getElementById('techtipicon');
+const autrestipIcon = document.getElementById('autrestipicon');
 const img1 = 'url("./assets/img/mouseicon.png")';
 const img2 = 'url("./assets/img/clickicon.png")';
 
@@ -6,5 +7,6 @@ let toggle = false;
 
 setInterval(() => {
     techtipIcon.style.backgroundImage = toggle ? img1 : img2;
+    autrestipIcon.style.backgroundImage = toggle ? img1 : img2;
     toggle = !toggle;
 }, 1000);
